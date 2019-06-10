627fe67123354dd53770d0b5551527645936b231
Michael Rubenstein <mrubenstein6192@gmail.com>
2019-06-10 16:56:43 -0400

    deploy test

--- controllers/hike-controller.js
+++ controllers/hike-controller.js
@@ -1,6 +1,6 @@
 //import Hike model
 const { Hike, User } = require('../models');
-const handle = require('../utils/promise-handler');
+
 
 // this runs when GET /api/hikes is hit
 const getSavedHikes = (req, res) => {
@@ -13,79 +13,38 @@ const getSavedHikes = (req, res) => {
 };
 
 // /api/hikes/:id
-const getHikeById  = async (req, res) => {
-  const [hikeFindErr, hikeData] = await handle(Hike.findById(req.params.id));
-
-  if (hikeFindErr) {
-    return res.status(500).json(userFindErr)
-  }
-
-  return res.status(200).json(hikeData) 
-};
+function getHikeById(req, res) {
+  Hike.findById(req.params.id)
+  .then(dbHikeData => res.json(dbHikeData))
+    .catch(err => {
+      console.log(err);
+      res.json(err);
+    });
+}
 
 
 // this runs when POST /api/hikes is hit
 //req.body => {hikeID: "", name: "", location: "", legnth in miles: "", time to complete: "", diffculty: "", dogs seen: "", overall experience: "" }
-const saveHike =  async (req, res) => {
-  const [hikeErr, hikeData] = await handle(Hike.create(req.body));
-
-  if (hikeErr) {
-    console.log(hikeErr);
-    return res.json(hikeErr);
-  };
-  
-  return User.findOneAndUpdate(
-    {
-      _id: req.user._id
-    },
-    {
-      $push: { hikes: hikeData._id }
-    },
-    {
-      new: true
-    },
-    {
-      hikeSaved: true
-    }
-  )
-    .then((userInfo) => {
-      if (userInfo !== null) {
-        return res.json(userInfo);
-      };
-    })
-    .catch((err) => {
-      return res.json(err);
+const saveHike = (req, res) => {
+  Hike.create(req.body)
+    .then(dbHikeData => res.json(dbHikeData))
+    .catch(err=> {
+      console.log(err);
+      res.json(err);
     });
 };
 
 
 //this will run when DELETE /api/hikes/:id is hit
-const removeHike = async (req, res) => {
-  const [hikeFindErr, hikeData] = await handle(Hike.findByIdAndDelete(req.params.id));
-
-  if (hikeFindErr) {
-    return res.status(500).json(hikeFindErr)
-  };
-
-  // delete hike from User schema
-  const [userFindErr, userData] = await handle(Hike.findByIdAndUpdate(
-    req.user._id,
-    {
-      $pull: {
-        hikes: req.params.id
-      }
-    },
-    {
-      new: true
-    }
-  ));
-
-  if (userFindErr) {
-    return res.status(500).json(userFindErr)
-  }
-
-  return res.status(200).json(userData) 
-
+const removeHike = (req, res) => {
+  Hike.remove({
+    _id: req.params.id
+  })
+    .then(dbHikeData => res.json(dbHikeData))
+    .catch(err => {
+      console.log(err);
+      res.json(err);
+    });
 };
 
 module.exports = {
--- controllers/planned-controller.js
+++ controllers/planned-controller.js
@@ -1,5 +1,4 @@
-const { Planned, User } = require('../models');
-const handle = require('../utils/promise-handler');
+const { Planned } = require('../models');
 
 // this runs when GET /api/hikes is hit
 const getPlannedHikes = (req, res) => {
@@ -12,78 +11,37 @@ const getPlannedHikes = (req, res) => {
 };
 
 // /api/hikes/:id
-const getPlannedHikeById  = async (req, res) => {
-  const [plannedFindErr, plannedData] = await handle(Planned.findById(req.params.id));
-
-  if (plannedFindErr) {
-    return res.status(500).json(userFindErr)
-  }
-
-  return res.status(200).json(plannedData) 
-};
+function getPlannedHikeById(req, res) {
+  Planned.findById(req.params.id)
+  .then(dbHikeData => res.json(dbHikeData))
+    .catch(err => {
+      console.log(err);
+      res.json(err);
+    });
+}
 
 
 // this runs when POST /api/hikes is hit
 //req.body => {hikeID: "", name: "", location: ""
-const planHike =  async (req, res) => {
-  const [plannedErr, plannedData] = await handle(Planned.create(req.body));
-
-  if (plannedErr) {
-    console.log(plannedErr);
-    return res.json(plannedErr);
-  };
-  
-  return User.findOneAndUpdate(
-    {
-      _id: req.user._id
-    },
-    {
-      $push: { plannedHikes: plannedData._id }
-    },
-    {
-      new: true
-    },
-    {
-      hikeSaved: true
-    }
-  )
-    .then((userInfo) => {
-      if (userInfo !== null) {
-        return res.json(userInfo);
-      };
-    })
-    .catch((err) => {
-      return res.json(err);
+const planHike = (req, res) => {
+  Planned.create(req.body)
+    .then(dbHikeData => res.json(dbHikeData))
+    .catch(err=> {
+      console.log(err);
+      res.json(err);
     });
 };
 
 //this will run when DELETE /api/hikes/:id is hit
-const removePlannedHike = async (req, res) => {
-  const [plannedFindErr, plannedData] = await handle(Planned.findByIdAndDelete(req.params.id));
-
-  if (plannedFindErr) {
-    return res.status(500).json(plannedFindErr)
-  };
-
-  // delete hike from User schema
-  const [userFindErr, userData] = await handle(Planned.findByIdAndUpdate(
-    req.user._id,
-    {
-      $pull: {
-        plannedHikes: req.params.id
-      }
-    },
-    {
-      new: true
-    }
-  ));
-
-  if (userFindErr) {
-    return res.status(500).json(userFindErr)
-  }
-
-  return res.status(200).json(userData) 
-
+const removePlannedHike = (req, res) => {
+  Planned.remove({
+    _id: req.params.id
+  })
+    .then(dbHikeData => res.json(dbHikeData))
+    .catch(err => {
+      console.log(err);
+      res.json(err);
+    });
 };
 
 module.exports = {
